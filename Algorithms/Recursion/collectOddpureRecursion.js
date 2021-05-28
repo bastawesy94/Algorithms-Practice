80df017afe3879d8b2b0c28127aa2728e0fe474c
bastawesy <mohamedel.bastawesy2020@gmail.com>
2021-05-28 16:12:48 +0200

    remanage files

--- Algorithms/Recursion/collectOddpureRecursion.js
+++ Algorithms/Recursion/collectOddpureRecursion.js
@@ -1,9 +1,7 @@
 function collectOdd(arr) {
     let result = [];
-    if(arr.length === 0)
-        return result ;
-    if(arr[0] %2 !== 0)
-        result.push(arr[0]);
+    if(arr.length === 0) return result;
+    if(arr[0] %2 !== 0) result.push(arr[0]);
     return result.concat(collectOdd(arr.slice(1)))
 }
 console.log(collectOdd([1,3,4,8])) ;
