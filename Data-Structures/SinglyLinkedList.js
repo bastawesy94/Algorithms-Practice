d51892eb81420b41bc7aac062859193cfdd6f0e4
bastawesy <mohamedel.bastawesy2020@gmail.com>
2021-08-29 00:14:40 +0200

    push and pop methods

--- Data-Structures/SinglyLinkedList.js
+++ Data-Structures/SinglyLinkedList.js
@@ -1,58 +1,60 @@
 //imlementation of node 
 class Node {
-    constructor(val, nextVal) {
+    constructor(val) {
         this.val = val
-        nextVal = null
+        this.nextVal = null
     }
 }
+//imlementation of Singly Linked List
 class SinglyLinkedList {
     constructor() {
         this.head = null
         this.tail = null
         this.length = 0
     }
+//****************instances methods*********************//    
     push(val) {
-        let node = new Node(val)
-        if (this.head === null) {
-            this.head = node
-            this.tail = this.head
-        }
-        else {
-            this.tail.nextVal = node
-            this.tail = node
-        }
-        this.length++
-        return this
+      const node = new Node(val);  
+      if(this.length == 0){
+          this.head = node
+          this.tail = node
+      }else{
+          this.tail.nextVal = node
+          this.tail = node
+      }
+      this.length ++
+      return this;
     }
     pup() {
-        if (!this.head)
-            return undefined
-
+        if(this.length ==0) return undefined
         let current = this.head
         let newTail = current
-        while (newTail.nextVal) {
-            current = newTail
-            newTail = current.nextVal
+        while(current.nextVal){
+            newTail = current
+            current = current.nextVal
         }
-
-        this.tail = current
+        this.tail = newTail
         this.tail.nextVal = null
-        this.length--
-        if (this.length == 0) {
+        this.length --
+        if(this.length ==0){
             this.head = null
             this.tail = null
         }
         return current
     }
+       
 }
 
 const l = new SinglyLinkedList()
-l.push("x")
-console.log(l)
-l.push("y")
-console.log(l)
-l.pup()
-console.log(l)
-l.pup()
-console.log(l)
 
+console.log(l.push("HI"))
+console.log(l.push("YOU"))
+console.log(l.push("!"))
+
+
+console.log(l.pup())
+console.log(l.pup())
+console.log(l.pup())
+console.log(l.pup())
+
+console.log(l)
